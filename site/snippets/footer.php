79c19d21e0f1478d9b4cb6e5cd29cf00e50494ab
Lukas Leitsch <lukas@leitsch.org>
2023-07-10 18:32:03 +0200

    add tailwindcss

---
+++ site/snippets/footer.php
@@ -0,0 +1,7 @@
+</main>
+
+<footer class="footer">
+</footer>
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './site/**/*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
